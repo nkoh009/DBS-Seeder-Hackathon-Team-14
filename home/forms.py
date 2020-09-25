8f2b39b712110f61a76f5d588f2d38fa5a1ae0e4
huichun66 <pewpewt97@gmail.com>
2020-09-25 14:59:13 +0800

    Modify change for views.py

--- home/forms.py
+++ home/forms.py
@@ -3,14 +3,13 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput)
-    )
     source = forms.CharField(       # A hidden input for internal use
         max_length=50,              # tell from which page the user sent the message
         widget=forms.HiddenInput()
     )
 
     def clean(self):
-        cleaned_data = super(ContactForm, self).clean()
+        cleaned_data = super(LoginForm, self).clean()
         username = cleaned_data.get('username')
         password = cleaned_data.get('password')
         if not username and not password:
